0dbc99f6ffefc2b32824c3dcf234786429bcbdd8
Kim Van Wyk <kmvanwyk@msn.com>
2023-01-17 20:33:20 -0800

    fixing server

--- public/js/login.js
+++ public/js/login.js
@@ -26,11 +26,12 @@ const loginFormHandler = async (event) => {
     const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    const isProvider = document.querySelector('#provider-signup').checked;
   
     if (name && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ name, email, password, isProvider }),
         headers: { 'Content-Type': 'application/json' },
       });
   

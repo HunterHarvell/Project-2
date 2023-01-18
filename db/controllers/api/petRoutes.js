60d74026111144376b0e6fb58c35c01726ffe0b7
walleric23 <walleric23@gmail.com>
2023-01-17 19:17:46 -0800

    pet route and util folder

---
+++ db/controllers/api/petRoutes.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const { Pet } = require("../../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const petData = await Pet.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const pets = petData.map((pet) => pet.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      projects,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPet = await Pet.Create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPet);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/id", withAuth, async (req, res) => {
+  try {
+    const petData = await Pet.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!petData) {
+      res.status(404).json({ message: "No pet was found :(" });
+      return;
+    }
+
+    res.status(200).json(petData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
